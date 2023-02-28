a946a09b5a6b0cc3a83dcb78023b26d81868b644
piuskevin3 <mpavatarpius@gmail.com>
2023-02-28 18:04:38 +0300

    Add get api

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { spawn } = require('child_process');
 const app = express();
 
+app.get('/run-python-script', (req, res) => {
+  
 const process = spawn('python', ['./ocrIndex.py']);
 
 const getPyData = (data) => {
-  console.log("9 : " + data);
+  return data
 }
 
 
@@ -22,30 +24,12 @@ process.stderr.on('data', (data) => {
 process.on('close', (code) => {
   console.log(`Script execution finished with code: ${code}`);
   // res.send(result);
+  res.json(getPyData)
 });
 
 
-/*
-app.get('/run-python-script', (req, res) => {
-  const process = spawn('python', ['./ocrIndex.py']);
-
-  let result = '';
-  process.stdout.on('data', (data) => {
-    result += data.toString();
-  });
-
-  process.stderr.on('data', (data) => {
-    console.error(`Error: ${data}`);
-    res.status(500).send(`Error: ${data}`);
-  });
-
-  process.on('close', (code) => {
-    console.log(`Script execution finished with code: ${code}`);
-    res.send(result);
-  });
 });
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
-*/
